7f271eabe2d28ed1a33a0401a88abfa3cf7c9fea
Anamitra Hazra <122734800+deadlovepoetry@users.noreply.github.com>
2025-05-24 11:01:57 +0530

    Update api.ts
    backend url added

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3001";
+export const API_BASE_URL = "https://shop-karo-eta.vercel.app/";
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/api/auth`,
